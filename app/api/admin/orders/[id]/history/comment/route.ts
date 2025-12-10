0d6076bf76fbd6189bb8bc0b793154cbe049d6aa
Mario Rico <ricco.mario@gmail.com>
2025-12-10 10:34:36 -0500

    fix(history comment): use app router getServerSession signature

--- app/api/admin/orders/[id]/history/comment/route.ts
+++ app/api/admin/orders/[id]/history/comment/route.ts
@@ -1,37 +1,39 @@
+// app/api/admin/orders/[id]/history/comment/route.ts
+
 import { prisma } from '@/lib/prisma'
 import { NextRequest, NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
-import { authOptions } from '@/lib/auth'
+import { authOptions } from '@/lib/authOptions'
 import { Role } from '@prisma/client'
 
 export async function POST(
   req: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const session = await getServerSession(req, authOptions)
-  const userEmail = session?.user?.email
-
-  if (!userEmail) {
-    return NextResponse.json({ message: 'Unauthorized' }, { status: 401 })
-  }
-
-  const user = await prisma.user.findUnique({
-    where: { email: userEmail },
-  })
-
-  if (!user) {
-    return NextResponse.json({ message: 'User not found' }, { status: 404 })
-  }
-
-  // Validar si el usuario es admin o superadmin
-  if (user.role !== Role.ADMIN && user.role !== Role.SUPERADMIN) {
-    return NextResponse.json({ message: 'Forbidden' }, { status: 403 })
-  }
-
-  const orderId = params.id
-  const { status, comment } = await req.json()
-
   try {
+    const session = await getServerSession(authOptions)
+    const userEmail = session?.user?.email
+
+    if (!userEmail) {
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 })
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { email: userEmail },
+    })
+
+    if (!user) {
+      return NextResponse.json({ message: 'User not found' }, { status: 404 })
+    }
+
+    // Validar si el usuario es admin o superadmin
+    if (user.role !== Role.ADMIN && user.role !== Role.SUPERADMIN) {
+      return NextResponse.json({ message: 'Forbidden' }, { status: 403 })
+    }
+
+    const orderId = params.id
+    const { status, comment } = await req.json()
+
     const newHistory = await prisma.orderHistory.create({
       data: {
         orderId,
